2c2491e1f7d9bd05db37304630ca0e933044d090
PATILSANTHOSH27 <patilsanthosh27@gmail.com>
2026-03-04 14:57:07 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,22 +1,18 @@
 import sqlite3
 
-# create database
 conn = sqlite3.connect("users.db")
 
 cursor = conn.cursor()
 
-# create users table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
-
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
-
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+email TEXT UNIQUE NOT NULL,
+password TEXT NOT NULL
 )
 """)
 
-print("Database created successfully!")
-
 conn.commit()
 conn.close()
+
+print("Database created successfully")
